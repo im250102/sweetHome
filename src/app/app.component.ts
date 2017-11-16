7b20dbdbd02d24f0628637eecc2f3e1a5ba4176c
im250102 <ignacio.martin1083@gmail.com>
2017-11-16 21:13:33 +0100

    First version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+sdfsdf
+
+import { HouseService } from './services/house.service';
+import { House } from './models/house';
+import { GLOBAL } from './services/global';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  title = 'Los Mejores Vinos';
+  public header_color:string;
+
+  	constructor(){
+  	
+  	}
+
+ 	  ngOnInit(){
+		    console.log('house.component.ts cargado');
+	  }
+
 }
